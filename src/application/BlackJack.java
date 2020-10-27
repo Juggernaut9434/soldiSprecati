e4734e657b51b8b8beb7df6637e25cfa03837f30
MikeMathews123 <juggernaut9434@gmail.com>
2020-10-27 16:56:45 -0700

    adjustments to BlackJack to fit JUnit tests

--- src/application/BlackJack.java
+++ src/application/BlackJack.java
@@ -29,6 +29,7 @@ public class BlackJack extends Table {
 		{
 			deck = new Deck();
 		}
+		playerHands = new ArrayList<ArrayList<Card>>();
 	}
 	
 	/***
@@ -40,37 +41,63 @@ public class BlackJack extends Table {
 	{
 		int numOfPlayers = super.players.size();
 		int[] playerScores = new int[numOfPlayers];		// instance with ints with the size of players
+		
 		// catch if no other players besides dealer are in
 		if(numOfPlayers<=1)
 			throw new InvalidLogicException("BlackJack.mainPlay():\tNo players to play"
 					+ "\nplayers:" + players);
+		
 		// deal 2 cards to every player and evaluate their round's cards value
 		// the row will be equivalent to the player's index
 		playerHands = deck.deal(2, numOfPlayers);
-		
-		// access the player then access card within player
-		int i=0;	// for the playerScores index iteration
-		for(ArrayList<Card> player: playerHands)
-		{
-			for(Card card: player)
-			{
-				playerScores[i] += this.getScoreVal(card);	// get total score of player and add to hand
-			}
-			i++;	// go to next player
-		}
+		scoreHands(playerScores);
 		
 		// ask player to place a bet	
 		// player Actions
+		for(int i=0;i<numOfPlayers;i++)
+		{
+			int state = 0;
+			while(state == 0)
+			{
+				//TODO ask for user input on action
+				break;
+			}
+			if(state == -1) return;	//TODO find a thing to show that they lost
+			if(state == 1) return;	//TODO find a thing to show that they stayed and then compared to dealer.
+		}
+		
+	}
+	
+	public void scoreHands(int[] playerScores)
+	{
+		// access the player then access card within player
+		int i=0;	// for the playerScores index iteration
+		for(ArrayList<Card> player: getPlayerHands())
+		{
+			for(Card card: player)
+			{
+				try {
+					playerScores[i] += this.getScoreVal(card);
+				} catch (InvalidLogicException e) {
+					e.printStackTrace();
+				}	// get total score of player and add to hand
+			}
+			i++;	// go to next player
+		}
 	}
 	
 	public int getScoreVal(Card card) throws InvalidLogicException
 	{
 		char r = card.getRank();
 		// r is a royal or ten
-		if(("TJQK".indexOf(r) != -1))
+		
+		if("23456789".indexOf(r) != -1)
+		{
+			int r0 = Integer.parseInt(String.valueOf(r));
+			return r0;
+		}
+		else if(("TJQK".indexOf(r) != -1))
 			return 10;
-		else if("23456789".indexOf(r) != -1)
-			return (int) r;
 		else if("A".indexOf(r) != -1)
 			return 11;
 		else
@@ -82,7 +109,7 @@ public class BlackJack extends Table {
 	*/
 	public void hit(int playerIndex)
 	{
-		playerHands.get(playerIndex).add(this.deck.getTopCard());
+		getPlayerHands().get(playerIndex).add(this.deck.getTopCard());
 	}
 	
 	/* + stay(int): boolean
@@ -90,11 +117,9 @@ public class BlackJack extends Table {
 	 * @param playerIndex: int, the row/player from playerHands
 	 * @return boolean: does player want to stay
 	 */
-	public boolean stay(int playerIndex)
+	public int stay(int playerIndex)
 	{
-		// TODO a better way of doing this
-		// possibly a value for each player
-		return true;
+		return 1;
 	}
 	/* +split(int): void - maybe not dev right away
 	 * checks if both cards are the same rank
@@ -105,29 +130,22 @@ public class BlackJack extends Table {
 	 * @param playerIndex: int - the row/player from playerHands
 	 * @return: boolean, if score is over 21, bust
 	 */
-	public boolean bust(int playerIndex, int[] scores)
+	public int isBust(int playerIndex, int[] scores)
 	{
 		if(scores[playerIndex] >= 21)
 		{
-			return true;
+			return -1;
 		}
-		return false;
+		return 0;
 	}
 	/* + bet(int): int, int - index of player, return be
 	 * Sets a bet for the game.
 	 * @param playerIndex: int - the row/player from playerHands
 	 * @return bet: int - the bet value
 	 */
-	
-	public static void main(String[] args)
-	{
-		BlackJack game = new BlackJack(5, 10, "John");
-		try {
-			game.addPlayer(new Gambler(10, "Henry"));
-			game.mainPlay();
-		} catch (InvalidLogicException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+	public ArrayList<ArrayList<Card>> getPlayerHands() {
+		return playerHands;
 	}
+	
 }
